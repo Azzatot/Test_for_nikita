b89e9b3baa9e015456f94935c653a2a650458b88
Danylo Korolenko <danylokorolenko74@gmail.com>
2023-09-25 16:02:57 +0300

    fix name + refactoring

--- test/test.py
+++ test/test.py
@@ -1,15 +1,15 @@
+from helpers import steps
 from base_driver import options
-from helpers.steps import uninstall, install, policy_manager_is_visible, open_app, close_app
 
 
 def test_app_reinstall():
-    uninstall(options)
-    install()
-    open_app(options)
-    policy_manager_is_visible()
+    steps.uninstall(options)
+    steps.install()
+    steps.open_app(options)
+    steps.policy_manager_is_visible()
 
 
 def test_app_closed():
-    close_app(options)
-    open_app(options)
-    policy_manager_is_visible()
+    steps.close_app(options)
+    steps.open_app(options)
+    steps.policy_manager_is_visible()
